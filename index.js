0d8725da3938d465676dac7f2e85adf1bdfd0b40
pedreviljoen <pedre87@gmail.com>
2019-02-11 14:17:04 +0200

    Added some prettier hooks

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const {missingFields} = require('./reader')
-const question = require('./config/question')
-const chalk = require('chalk')
-const figlet = require('figlet')
-const inquirer = require('inquirer')
-const colors = require('colors')
+const { missingFields } = require("./reader")
+const question = require("./config/question")
+const chalk = require("chalk")
+const figlet = require("figlet")
+const inquirer = require("inquirer")
+const colors = require("colors")
 
 const init = () => {
-    console.log(
-        chalk.green(
-          figlet.textSync("Package Police", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default"
-          })
-        )
-      )
+  console.log(
+    chalk.green(
+      figlet.textSync("Package Police", {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+      })
+    )
+  )
 }
 
 const askChecker = () => {
-    return inquirer.prompt(question)
+  return inquirer.prompt(question)
 }
 
 const run = async () => {
-    init()
-    const answer = await askChecker()
-    const { CHECKER } = answer
-    
-    if ( CHECKER.toLowerCase() == 'y' ) {
-        const missing = await missingFields()
+  init()
+  const answer = await askChecker()
+  const { CHECKER } = answer
 
-        if (missing.length > 0) {
-            console.log('We recommend you add the following fields: ')
-            missing.forEach(item => console.log(colors.red.bold(item)))
-        } else {
-            console.log(colors.green('Great! Seems like you are up to scratch.'))
-        }
+  if (CHECKER.toLowerCase() == "y") {
+    const missing = await missingFields()
+
+    if (missing.length > 0) {
+      console.log("We recommend you add the following fields: ")
+      missing.forEach(item => console.log(colors.red.bold(item)))
+    } else {
+      console.log(colors.green("Great! Seems like you are up to scratch."))
     }
+  }
 }
 
 run()
