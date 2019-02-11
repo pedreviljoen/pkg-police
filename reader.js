18e17e110154bc2d2dd97878a6c030b1d053c91f
pedreviljoen <pedre87@gmail.com>
2019-02-11 15:06:35 +0200

    Wrote up some tests closes #2

--- reader.js
+++ reader.js
@@ -1,16 +1,15 @@
 const readPkgUp = require('read-pkg-up')
-const recommended = require('./config/recommended')
 
 async function readContents () {
     const contents = await readPkgUp()
     return contents
 }
 
-async function missingFields () {
+async function missingFields (input) {
     const contents = await readContents()
     const fields = Object.keys(contents.pkg)
 
-    return recommended.filter(item => !fields.includes(item))
+    return input.filter(item => !fields.includes(item))
 }
 
 module.exports = {
